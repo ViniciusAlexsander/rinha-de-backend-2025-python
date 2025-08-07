ed995e85ed4d5e58ab95883713488e5252cca27b
Vinicius Marinho <viniciusalexsander1@gmail.com>
2025-08-07 16:45:23 -0300

    fazendo a api esperar pelo banco

--- app/db.py
+++ app/db.py
@@ -1,3 +1,4 @@
+import asyncio
 import asyncpg
 import os
 from dotenv import load_dotenv
@@ -9,4 +10,11 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 print("DATABASE_URL =", DATABASE_URL)
 
 async def get_db_pool():
-    return await asyncpg.create_pool(DATABASE_URL)
+    retries = 10
+    for i in range(retries):
+        try:
+            return await asyncpg.create_pool(DATABASE_URL)
+        except Exception as e:
+            print(f"Attempt {i + 1} failed: {e}")
+            await asyncio.sleep(2)
+    raise Exception("Could not connect to the database after several attempts")
