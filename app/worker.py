10b48f845e3bb537c7298ddbf0a14f069884482a
Vinicius Marinho <viniciusalexsander1@gmail.com>
2025-08-11 23:21:05 -0300

    fix: add requisicoes em paralelo

--- app/worker.py
+++ app/worker.py
@@ -1,17 +1,35 @@
 from app.queue import payment_queue
 from app.processor import process_payment
 import logging
+import asyncio
 
 logger = logging.getLogger("uvicorn.error")
 
+MAX_CONCURRENT_PAYMENTS = 50
+
+async def handle_payment(pagamento, db_pool):
+    try:
+        amount = pagamento["amount"]
+        correlationId = pagamento["correlationId"]
+        await process_payment(amount, correlationId, db_pool)
+    except Exception as e:
+        logger.error("Erro ao processar pagamento: %s", e)
+    finally:
+        payment_queue.task_done()
+
 async def payment_worker(db_pool):
-    while True:
-        try:
+    semaphore = asyncio.Semaphore(MAX_CONCURRENT_PAYMENTS)
+
+    async def worker_task():
+        while True:
             pagamento = await payment_queue.get()
-            amount = pagamento["amount"]
-            correlationId = pagamento["correlationId"]
-            await process_payment(amount, correlationId, db_pool)
-        except Exception as e:
-            logger.error("Erro ao processar pagamento na fila: %s", e)
-        finally:
-            payment_queue.task_done()
+            await semaphore.acquire()
+            asyncio.create_task(process_with_semaphore(pagamento, db_pool))
+
+    await worker_task()
+
+async def process_with_semaphore(pagamento, db_pool, semaphore):
+    try:
+        await handle_payment(pagamento, db_pool)
+    finally:
+        semaphore.release()
